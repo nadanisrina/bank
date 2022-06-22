4957ec8bc16d6c4660e0e71f8e8f5e5e0e371b01
nadans <nisrinaseptiana1@gmail.com>
2022-06-22 19:50:58 +0700

    docs: add helper error and swagger

--- handler/user.go
+++ handler/user.go
@@ -16,16 +16,32 @@ func NewUserHandler(userService user.Service) *userHandler {
 	return &userHandler{userService}
 }
 
+// CreateUser godoc
+// @Summary      Create User
+// @Description  create user
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        id   path      object  true  "user.RegisterUserInput"
+// @Success      200  {object}  user.User
+// @Failure      400  {object}  helper.HTTPError
+// @Failure      404  {object}  helper.HTTPError
+// @Failure      500  {object}  helper.HTTPError
+// @Router       /users [post]
 func (h *userHandler) RegisterUser(c *gin.Context) {
 	//take input from user
 	var input user.RegisterUserInput
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
+		helper.NewError(c, http.StatusBadRequest, err)
+		return
 	}
 	newUser, err := h.userService.RegisterUserInput(input)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, err)
+		helper.NewError(c, http.StatusBadRequest, err)
+		return
 	}
 	// token,err := h.jwtService.GenerateToken()
 	formatted := user.FormatUser(newUser, "token")
--- main.go
+++ main.go
@@ -5,12 +5,39 @@ import (
 	"bank/user"
 	"log"
 
+	"bank/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// @title           Bank API
+// @version         1.0
+// @description     This is a Bank server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
 func main() {
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "Sample of Bank Server"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "bank.swagger.io"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	dsn := "host=localhost user=postgres password=postgres dbname=bank port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
@@ -28,7 +55,7 @@ func main() {
 	{
 		v1.POST("/user", userHandler.RegisterUser)
 	}
-
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
 
 }
